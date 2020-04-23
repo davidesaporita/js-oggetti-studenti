bf3cd9dfd769c2ce6b7872e73ec993061a50a622
Davide Saporita <davide.saporita@gmail.com>
2020-04-23 17:34:57 +0200

    Chat added (hyper basic mode)

--- script/main.js
+++ script/main.js
@@ -21,7 +21,62 @@ $(document).ready( function() {
     };
 
     for ( var key in booleanStudent ) {
-        $('#task-1').append('<h5>' + key + ': ' + booleanStudent[key] + '</h5>');
+        $('#task-1').append('<span>' + key + ': ' + booleanStudent[key] + '</span><span>' + ' // ' + '<span>');
+    }
+
+    /* CHAT */
+    
+    // Refs
+    var input = $('.msg-input');
+    var button = $('.msg-button');
+    var chat = $('.chat');
+
+    // Init Handlebars
+    var source = $('#msg-template').html();
+    var template = Handlebars.compile(source);
+    
+    // Inserisci nuovo messaggio
+    button.click( function() {
+
+        var msg = input.val().trim();
+        
+        // Dati dinamici preparati per il template Handlebars
+        var data = {
+            text: msg,
+            way: 'sent',
+            time: getTime()
+        };
+
+        // Bot response
+        setTimeout(function() {
+
+            // Variables
+            var answer = 'Osserva attentamente, la risposta è nella domanda stessa';
+
+            var data = {
+                text: answer,
+                way: 'received',
+                time: getTime()
+            };
+            chat.append(template(data));            
+        }, 1000);
+
+        var html = template(data);
+        chat.append(html);
+        input.val('');
+    });
+
+    // Generazione orario
+    function getTime() {
+        var date = new Date();
+        var hours = addZero(date.getHours());
+        var minutes = addZero(date.getMinutes());
+        return hours + ':' + minutes;
+    }
+
+    // Supporto a getTime per aggiungere 0 prima di ore/minuti
+    function addZero(num) {
+        return (num < 10 ? ('0' + num) : num);
     }
 
 });
