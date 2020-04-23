dae441e6d39a6c636abbc4332396314a7dd90584
Davide Saporita <davide.saporita@gmail.com>
2020-04-23 14:44:50 +0200

    Added project files

---
+++ script/main.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * Oggetti
+ * Descrizione:
+ * Creare un oggetto che descriva uno studente
+ * lo studente avrà  le seguenti proprietà: nome, cognome e età.
+ * Stampare attraverso il for..in tutte le proprietà (chiavi e valori).
+ * 
+ * OPZIONALE (in una cartella a parte dal codice per l’esercizio degli oggetti)
+ * Riprodurre la piccola chat fatta insieme durante la lezione utilizzando handlerbars.
+ * 
+ */
+
+
